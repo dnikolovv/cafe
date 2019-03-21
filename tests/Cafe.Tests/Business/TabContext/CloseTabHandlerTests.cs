e7920bfdf73243ecbd45efb79b04c88d12e9ec41
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-03-21 11:37:52 +0200

    More tests.

--- tests/Cafe.Tests/Business/TabContext/CloseTabHandlerTests.cs
+++ tests/Cafe.Tests/Business/TabContext/CloseTabHandlerTests.cs
@@ -1,8 +1,10 @@
 ﻿using Cafe.Core.TabContext.Commands;
 using Cafe.Core.TableContext.Commands;
 using Cafe.Core.WaiterContext.Commands;
+using Cafe.Domain;
 using Cafe.Tests.Business.TabContext.Helpers;
 using Cafe.Tests.Customizations;
+using Cafe.Tests.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -44,5 +46,29 @@ namespace Cafe.Tests.Business.TabContext
                 t => t.IsOpen == false &&
                      t.TipValue == 0);
         }
+
+        [Theory]
+        [CustomizedAutoData]
+        public async Task CannotCloseATabTwice(Guid tabId, int tableNumber)
+        {
+            // Arrange
+            await _helper.OpenTabOnTable(tabId, tableNumber);
+
+            var closeTabCommand = new CloseTab
+            {
+                TabId = tabId,
+                AmountPaid = 0 // This should be OK as we haven't ordered anything
+            };
+
+            await _fixture.SendAsync(closeTabCommand);
+
+            // Act
+            var result = await _fixture.SendAsync(closeTabCommand);
+
+            // Assert
+            result.ShouldHaveErrorOfType(ErrorType.Validation);
+        }
+
+        // TODO: After implementing ServeMenuItems, add tests that check whether you can close a tab paying less than the owed amount
     }
 }
