d0b9f750ad93e44efd5e9ef33e795eeaa1496340
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-03-25 13:45:36 +0200

    Waiter endpoints.

---
+++ src/server/Cafe.Api/Controllers/WaiterController.cs
@@ -0,0 +1,33 @@
+﻿using Cafe.Core.WaiterContext.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Cafe.Api.Controllers
+{
+    public class WaiterController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public WaiterController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Hires a waiter in the café.
+        /// </summary>
+        [HttpPost("hire")]
+        public async Task<IActionResult> HireWaiter([FromBody] HireWaiter command) =>
+            (await _mediator.Send(command))
+            .Match(Ok, Error);
+
+        /// <summary>
+        /// Assigns a table to a waiter.
+        /// </summary>
+        [HttpPost("table/assign")]
+        public async Task<IActionResult> AssignTable([FromBody] AssignTable command) =>
+            (await _mediator.Send(command))
+            .Match(Ok, Error);
+    }
+}
