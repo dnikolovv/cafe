f04c806ab6e6ef0a4d55ef05df7414f0fd2fc043
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-03-25 13:37:31 +0200

    Tab endpoints.

---
+++ src/server/Cafe.Api/Controllers/TabController.cs
@@ -0,0 +1,49 @@
+﻿using Cafe.Core.TabContext.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Cafe.Api.Controllers
+{
+    public class TabController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public TabController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Opens a new tab on a given table.
+        /// </summary>
+        [HttpPost("open")]
+        public async Task<IActionResult> OpenTab([FromBody] OpenTab command) =>
+            (await _mediator.Send(command))
+            .Match(Ok, Error);
+
+        /// <summary>
+        /// Closes a tab.
+        /// </summary>
+        [HttpPost("close")]
+        public async Task<IActionResult> CloseTab([FromBody] CloseTab command) =>
+            (await _mediator.Send(command))
+            .Match(Ok, Error);
+
+        /// <summary>
+        /// Orders a list of menu items for a given tab.
+        /// </summary>
+        [HttpPost("order")]
+        public async Task<IActionResult> OrderMenuItems([FromBody] OrderMenuItems command) =>
+            (await _mediator.Send(command))
+            .Match(Ok, Error);
+
+        /// <summary>
+        /// Rejects a list of menu items for a given tab.
+        /// </summary>
+        [HttpPost("reject")]
+        public async Task<IActionResult> RejectMenuItems([FromBody] RejectMenuItems command) =>
+            (await _mediator.Send(command))
+            .Match(Ok, Error);
+    }
+}
