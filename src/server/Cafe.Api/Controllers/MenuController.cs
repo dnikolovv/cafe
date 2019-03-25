b01183e305a69f5625b58dfd16588a5051d8e8d6
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-03-25 13:30:49 +0200

    Add menu items endpoint.

---
+++ src/server/Cafe.Api/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using Cafe.Core.MenuContext.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Cafe.Api.Controllers
+{
+    public class MenuController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public MenuController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Adds items to the menu.
+        /// </summary>
+        /// <param name="command">The items to add.</param>
+        [HttpPost("items")]
+        public async Task<IActionResult> AddMenuItems([FromBody] AddMenuItems command) =>
+            (await _mediator.Send(command))
+            .Match(Ok, Error);
+    }
+}
