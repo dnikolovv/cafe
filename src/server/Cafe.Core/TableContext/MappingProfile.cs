6b769f447ccfbeb119b1b82a2ed1b1be97448581
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-03-26 10:55:15 +0200

    Implemented GetAllTables query.

---
+++ src/server/Cafe.Core/TableContext/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Cafe.Domain.Entities;
+using Cafe.Domain.Views;
+
+namespace Cafe.Core.TableContext
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Table, TableView>(MemberList.Destination);
+        }
+    }
+}
---
+++ tests/Cafe.Tests/Business/TableContext/GetAllTablesHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using Cafe.Core.TableContext.Queries;
+using Cafe.Domain.Entities;
+using Cafe.Tests.Customizations;
+using Shouldly;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cafe.Tests.Business.TableContext
+{
+    public class GetAllTablesHandlerTests : ResetDatabaseLifetime
+    {
+        private readonly SliceFixture _fixture;
+
+        public GetAllTablesHandlerTests()
+        {
+            _fixture = new SliceFixture();
+        }
+
+        [Theory]
+        [CustomizedAutoData]
+        public async Task CanGetAllTables(GetAllTables query, Table[] tablesToAdd)
+        {
+            // Arrange
+            await _fixture.ExecuteDbContextAsync(async dbContext =>
+            {
+                var waiters = tablesToAdd
+                    .Select(t => t.Waiter);
+
+                dbContext.Waiters.AddRange(waiters);
+                dbContext.Tables.AddRange(tablesToAdd);
+
+                await dbContext.SaveChangesAsync();
+            });
+
+            // Act
+            var result = await _fixture.SendAsync(query);
+
+            // Assert
+            result.Exists(tables =>
+                tables.Count == tablesToAdd.Length &&
+                tables.All(t => tablesToAdd.Any(addedTable => t.Number == addedTable.Number &&
+                                                              t.WaiterId == addedTable.WaiterId &&
+                                                              t.WaiterShortName == addedTable.Waiter.ShortName)))
+            .ShouldBeTrue();
+        }
+    }
+}
