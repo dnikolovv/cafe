45e70d94f0ed211abf912c6b943edad678a95b5b
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-03-28 13:57:21 +0200

    First failing test for querying user accounts.

---
+++ src/server/Cafe.Core/AuthContext/Queries/GetAllUserAccounts.cs
@@ -0,0 +1,9 @@
+﻿using Cafe.Domain.Views;
+using System.Collections.Generic;
+
+namespace Cafe.Core.AuthContext.Queries
+{
+    public class GetAllUserAccounts : IQuery<IEnumerable<UserView>>
+    {
+    }
+}
---
+++ tests/Cafe.Tests/Business/AuthContext/GetAllUserAccountsHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture.Xunit2;
+using Cafe.Core.AuthContext.Commands;
+using Cafe.Core.AuthContext.Queries;
+using Shouldly;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cafe.Tests.Business.AuthContext
+{
+    public class GetAllUserAccountsHandlerTests : ResetDatabaseLifetime
+    {
+        private readonly SliceFixture _fixture;
+
+        public GetAllUserAccountsHandlerTests()
+        {
+            _fixture = new SliceFixture();
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task CanGetAllUserAccounts(Register[] registerAccountsCommands)
+        {
+            // Arrange
+            foreach (var command in registerAccountsCommands)
+            {
+                await _fixture.SendAsync(command);
+            }
+
+            var query = new GetAllUserAccounts();
+
+            // Act
+            var result = await _fixture.SendAsync(query);
+
+            // Assert
+            result.Exists(accounts =>
+            {
+                return accounts.All(a => registerAccountsCommands.Any(registeredAccount =>
+                    a.Id == registeredAccount.Id &&
+                    a.FirstName == registeredAccount.FirstName &&
+                    a.LastName == registeredAccount.LastName &&
+                    a.Email == registeredAccount.Email));
+            })
+            .ShouldBeTrue();
+        }
+    }
+}
