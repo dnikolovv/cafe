8f69263685df32a650a00ea5379fd7df1360a238
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-03-28 15:39:41 +0200

    First failing test for querying open tabs.

---
+++ tests/Cafe.Tests/Business/TabContext/GetAllOpenTabsHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using Cafe.Core.TabContext.Queries;
+using Cafe.Tests.Business.TabContext.Helpers;
+using Cafe.Tests.Customizations;
+using Shouldly;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cafe.Tests.Business.TabContext
+{
+    public class GetAllOpenTabsHandlerTests : ResetDatabaseLifetime
+    {
+        private readonly SliceFixture _fixture;
+        private readonly TabTestsHelper _helper;
+
+        public GetAllOpenTabsHandlerTests(SliceFixture fixture)
+        {
+            _fixture = fixture;
+            _helper = new TabTestsHelper(_fixture);
+        }
+
+        [Theory]
+        [CustomizedAutoData]
+        public async Task CanGetAllOpenTabs(Guid[] tabIds)
+        {
+            // Arrange
+            for (int i = 0; i < tabIds.Length; i++)
+            {
+                await _helper.OpenTabOnTable(tabIds[i], tableNumber: i + 1);
+            }
+
+            // Act
+            var result = await _fixture.SendAsync(new GetAllOpenTabs());
+
+            // Assert
+            result.Exists(tabs => tabs.Count == tabIds.Length &&
+                                  tabs.All(t => tabIds.Contains(t.Id)))
+            .ShouldBeTrue();
+        }
+    }
+}
