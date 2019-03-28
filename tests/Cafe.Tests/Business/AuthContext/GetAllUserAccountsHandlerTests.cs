a7dd409590691341dcbb7816374184df50b0e8fc
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-03-28 14:04:33 +0200

    Failing test for whether managers and waiters are mapped correctly.

--- tests/Cafe.Tests/Business/AuthContext/GetAllUserAccountsHandlerTests.cs
+++ tests/Cafe.Tests/Business/AuthContext/GetAllUserAccountsHandlerTests.cs
@@ -1,6 +1,7 @@
-﻿using AutoFixture.Xunit2;
-using Cafe.Core.AuthContext.Commands;
+﻿using Cafe.Core.AuthContext.Commands;
 using Cafe.Core.AuthContext.Queries;
+using Cafe.Domain.Entities;
+using Cafe.Tests.Customizations;
 using Shouldly;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,7 +19,7 @@ namespace Cafe.Tests.Business.AuthContext
         }
 
         [Theory]
-        [AutoData]
+        [CustomizedAutoData]
         public async Task CanGetAllUserAccounts(Register[] registerAccountsCommands)
         {
             // Arrange
@@ -40,6 +41,65 @@ namespace Cafe.Tests.Business.AuthContext
                     a.FirstName == registeredAccount.FirstName &&
                     a.LastName == registeredAccount.LastName &&
                     a.Email == registeredAccount.Email));
+            })
+            .ShouldBeTrue();
+        }
+
+        [Theory]
+        [CustomizedAutoData]
+        public async Task CanGetAllUserAccountsAndCheckForManagers(
+            Manager managerToAssign,
+            Waiter waiterToAssign,
+            Register managerAccount,
+            Register waiterAccount,
+            Register[] registerUnassignedAccountsCommands)
+        {
+            // Arrange
+            await _fixture.ExecuteDbContextAsync(async dbContext =>
+            {
+                dbContext.Managers.Add(managerToAssign);
+                dbContext.Waiters.Add(waiterToAssign);
+                await dbContext.SaveChangesAsync();
+            });
+
+            foreach (var command in registerUnassignedAccountsCommands)
+            {
+                await _fixture.SendAsync(command);
+            }
+
+            await _fixture.SendAsync(managerAccount);
+            await _fixture.SendAsync(waiterAccount);
+
+            await _fixture.SendAsync(new AssignManagerToAccount { AccountId = managerAccount.Id, ManagerId = managerToAssign.Id });
+            await _fixture.SendAsync(new AssignWaiterToAccount { AccountId = waiterAccount.Id, WaiterId = waiterToAssign.Id });
+
+            var query = new GetAllUserAccounts();
+
+            // Act
+            var result = await _fixture.SendAsync(query);
+
+            // Assert
+            result.Exists(accounts =>
+            {
+                var allUnassignedAccountsAreMappedCorrectly = accounts
+                    .All(a => registerUnassignedAccountsCommands.Any(registeredAccount =>
+                        a.Id == registeredAccount.Id &&
+                        a.FirstName == registeredAccount.FirstName &&
+                        a.LastName == registeredAccount.LastName &&
+                        a.Email == registeredAccount.Email &&
+
+                        // Very important
+                        a.IsManager == false &&
+                        a.ManagerId == null &&
+                        a.IsWaiter == false &&
+                        a.WaiterId == null));
+
+                var managerAccountMappedCorrectly = accounts.SingleOrDefault(a => a.Id == managerAccount.Id && a.IsManager && a.ManagerId == managerToAssign.Id) != null;
+                var waiterAccountMappedCorrectly = accounts.SingleOrDefault(a => a.Id == waiterAccount.Id && a.IsWaiter && a.WaiterId == waiterToAssign.Id) != null;
+
+                return allUnassignedAccountsAreMappedCorrectly &&
+                    managerAccountMappedCorrectly &&
+                    waiterAccountMappedCorrectly;
             })
             .ShouldBeTrue();
         }
