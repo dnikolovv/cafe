11566f345e73eb1e2c1afc6eda890be9fa52807b
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-03-29 13:49:13 +0200

    Exposed endpoints for querying and hiring cashiers.

---
+++ src/server/Cafe.Api/Controllers/CashierController.cs
@@ -0,0 +1,38 @@
+﻿using Cafe.Core.AuthContext;
+using Cafe.Core.CashierContext.Commands;
+using Cafe.Core.CashierContext.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Cafe.Api.Controllers
+{
+    public class CashierController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public CashierController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Retrieves a list of all currently employed cashiers in the café.
+        /// </summary>
+        [HttpGet]
+        [Authorize(Policy = AuthConstants.Policies.IsAdminOrManager)]
+        public async Task<IActionResult> GetEmployedCashiers() =>
+            (await _mediator.Send(new GetEmployedCashiers()))
+            .Match(Ok, Error);
+
+        /// <summary>
+        /// Hires a cashier in the café.
+        /// </summary>
+        [HttpPost("hire")]
+        [Authorize(Policy = AuthConstants.Policies.IsAdminOrManager)]
+        public async Task<IActionResult> HireCashier([FromBody] HireCashier command) =>
+            (await _mediator.Send(command))
+            .Match(Ok, Error);
+    }
+}
