8c0ee959c4cd240c61e2d31fd06affc62bff8e24
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-04-08 11:53:37 +0300

    Implemented GetEmployedManagers.

---
+++ src/server/Cafe.Business/ManagerContext/QueryHandlers/GetEmployedManagersHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Cafe.Core;
+using Cafe.Core.ManagerContext.Queries;
+using Cafe.Domain;
+using Cafe.Domain.Views;
+using Cafe.Persistance.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Optional;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cafe.Business.ManagerContext.QueryHandlers
+{
+    public class GetEmployedManagersHandler : IQueryHandler<GetEmployedManagers, IList<ManagerView>>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetEmployedManagersHandler(ApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<Option<IList<ManagerView>, Error>> Handle(GetEmployedManagers request, CancellationToken cancellationToken)
+        {
+            var managers = await _dbContext
+                .Managers
+                .ProjectTo<ManagerView>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            return managers
+                .Some<IList<ManagerView>, Error>();
+        }
+    }
+}
--- tests/Cafe.Tests/Business/ManagerContext/GetEmployedManagersHandlerTests.cs
+++ tests/Cafe.Tests/Business/ManagerContext/GetEmployedManagersHandlerTests.cs
@@ -2,10 +2,7 @@
 using Cafe.Domain.Entities;
 using Cafe.Tests.Customizations;
 using Shouldly;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
