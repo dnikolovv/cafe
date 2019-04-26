ce166e2cf64a4989ae93af0583c60a7b03e8e200
dnikolovv <dnikolovv@hotmail.com>
2019-04-26 10:50:09 +0300

    The database is now going to be reverted to an initial mock state each time the application starts.

--- server/src/server/Cafe.Api/Configuration/AuthConfiguration.cs
+++ server/src/server/Cafe.Api/Configuration/AuthConfiguration.cs
@@ -1,9 +1,11 @@
 ﻿using Cafe.Core.AuthContext;
 using Cafe.Domain.Entities;
+using Cafe.Persistance.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -11,6 +13,86 @@ namespace Cafe.Api.Configuration
 {
     public static class AuthConfiguration
     {
+        public static void RevertDatabaseToInitialState(this IApplicationBuilder app, ApplicationDbContext dbContext)
+        {
+            var menuItems = new List<MenuItem>
+            {
+                new MenuItem
+                {
+                    Id = Guid.NewGuid(),
+                    Description = "Cofee",
+                    Number = 1,
+                    Price = 1
+                },
+                new MenuItem
+                {
+                    Id = Guid.NewGuid(),
+                    Description = "Tea",
+                    Number = 2,
+                    Price = 1
+                },
+                new MenuItem
+                {
+                    Id = Guid.NewGuid(),
+                    Description = "Coke",
+                    Number = 3,
+                    Price = 2
+                },
+            };
+
+            var waiters = new List<Waiter>
+            {
+                new Waiter
+                {
+                    Id = Guid.NewGuid(),
+                    ShortName = "Pete"
+                }
+            };
+
+            var tables = new List<Table>
+            {
+                new Table
+                {
+                    Id = Guid.NewGuid(),
+                    Number = 1,
+                    WaiterId = waiters[0].Id
+                }
+            };
+
+            var baristas = new List<Barista>
+            {
+                new Barista
+                {
+                    Id = Guid.NewGuid(),
+                    ShortName = "John"
+                }
+            };
+
+            var cashiers = new List<Cashier>
+            {
+                new Cashier
+                {
+                    Id = Guid.NewGuid(),
+                    ShortName = "Steve"
+                }
+            };
+
+            dbContext.MenuItems.RemoveRange(dbContext.MenuItems);
+            dbContext.Waiters.RemoveRange(dbContext.Waiters);
+            dbContext.Tables.RemoveRange(dbContext.Tables);
+            dbContext.Baristas.RemoveRange(dbContext.Baristas);
+            dbContext.Cashiers.RemoveRange(dbContext.Cashiers);
+            dbContext.ToGoOrders.RemoveRange(dbContext.ToGoOrders);
+
+            dbContext.MenuItems.AddRange(menuItems);
+            dbContext.Waiters.AddRange(waiters);
+            dbContext.Tables.AddRange(tables);
+            dbContext.Baristas.AddRange(baristas);
+            dbContext.Cashiers.AddRange(cashiers);
+
+            dbContext.SaveChanges();
+        }
+
         public static async Task AddDefaultAdminAccountIfNoneExisting(this IApplicationBuilder app, UserManager<User> userManager, IConfiguration configuration)
         {
             var adminSection = configuration.GetSection("DefaultAdminAccount");
