fc2a21c1b57eee9237d5cff7e17323654b0353fe
dnikolovv <dnikolovv@hotmail.com>
2019-04-28 23:03:33 +0300

    Added a TestPriority attribute and a health check.

---
+++ server/src/server/Cafe.Api/Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cafe.Api.Controllers
+{
+    public class HealthController : ApiController
+    {
+        [HttpGet]
+        public IActionResult Get() =>
+            Ok();
+    }
+}
---
+++ server/tests/Cafe.Tests/HealthTests.cs
@@ -0,0 +1,33 @@
+﻿using Cafe.Tests.XUnit;
+using Shouldly;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cafe.Tests
+{
+    public class HealthTests
+    {
+        private readonly SliceFixture _fixture;
+
+        public HealthTests()
+        {
+            _fixture = new SliceFixture();
+        }
+
+        [Fact]
+        [TestPriority(-100)]
+        public async Task CanConnectToTestServer()
+        {
+            // Arrange
+            var client = new HttpClient();
+
+            // Act
+            var result = await client.GetAsync(_fixture.GetFullServerUrl("api/health"));
+
+            // Assert
+            result.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+    }
+}
