d1097b9405347f0473edc04b979d424a6fc1b23c
Dobromir Nikolov <dnikolovv@hotmail.com>
2019-05-09 10:57:14 +0300

    Header cosmetic improvements.

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -7,68 +7,80 @@ const Header = ({ isLoggedIn, currentUser }) => {
   const activeStyle = { color: "#000000" };
 
   return (
-    <nav>
-      <NavLink to="/" activeStyle={activeStyle} exact>
-        Home
-      </NavLink>
-      {" | "}
-      {isLoggedIn ? (
-        <>
-          {currentUser.isAdmin && (
-            <>
-              <NavLink to="/admin" activeStyle={activeStyle}>
-                Admin
-              </NavLink>
-              {" | "}
-            </>
-          )}
-          {(currentUser.isAdmin || currentUser.isCashier) && (
-            <>
-              <NavLink to="/cashier" activeStyle={activeStyle}>
-                Cashier
-              </NavLink>
-              {" | "}
-            </>
-          )}
-          {(currentUser.isAdmin || currentUser.isBarista) && (
-            <>
-              <NavLink to="/barista" activeStyle={activeStyle}>
-                Barista
-              </NavLink>
-              {" | "}
-            </>
-          )}
-          {(currentUser.isAdmin || currentUser.isManager) && (
-            <>
-              <NavLink to="/manager" activeStyle={activeStyle}>
-                Manager
-              </NavLink>
-              {" | "}
-            </>
-          )}
-          {(currentUser.isAdmin || currentUser.isWaiter) && (
-            <>
-              <NavLink to="/waiter" activeStyle={activeStyle}>
-                Waiter
-              </NavLink>
-              {" | "}
-            </>
-          )}
-          <NavLink to="/logout" activeStyle={activeStyle}>
-            Logout
+    <>
+      <div className="row">
+        <nav className="col-md-12 pb-0">
+          <NavLink to="/" activeStyle={activeStyle} exact>
+            Home
           </NavLink>
-        </>
-      ) : (
-        <NavLink to="/login" activeStyle={activeStyle}>
-          Login
-        </NavLink>
-      )}
-    </nav>
+          {" | "}
+          {isLoggedIn ? (
+            <>
+              {currentUser.isAdmin && (
+                <>
+                  <NavLink to="/admin" activeStyle={activeStyle}>
+                    Admin
+                  </NavLink>
+                  {" | "}
+                </>
+              )}
+              {(currentUser.isAdmin || currentUser.isCashier) && (
+                <>
+                  <NavLink to="/cashier" activeStyle={activeStyle}>
+                    Cashier
+                  </NavLink>
+                  {" | "}
+                </>
+              )}
+              {(currentUser.isAdmin || currentUser.isBarista) && (
+                <>
+                  <NavLink to="/barista" activeStyle={activeStyle}>
+                    Barista
+                  </NavLink>
+                  {" | "}
+                </>
+              )}
+              {(currentUser.isAdmin || currentUser.isManager) && (
+                <>
+                  <NavLink to="/manager" activeStyle={activeStyle}>
+                    Manager
+                  </NavLink>
+                  {" | "}
+                </>
+              )}
+              {(currentUser.isAdmin || currentUser.isWaiter) && (
+                <>
+                  <NavLink to="/waiter" activeStyle={activeStyle}>
+                    Waiter
+                  </NavLink>
+                  {" | "}
+                </>
+              )}
+              <NavLink to="/logout" activeStyle={activeStyle}>
+                Logout
+              </NavLink>
+            </>
+          ) : (
+            <NavLink to="/login" activeStyle={activeStyle}>
+              Login
+            </NavLink>
+          )}
+
+          {currentUser && currentUser.firstName && (
+            <div className="pull-right">
+              Hi, {currentUser.firstName + " " + currentUser.lastName + "!"}
+            </div>
+          )}
+        </nav>
+        <hr className="col-md-12" />
+      </div>
+    </>
   );
 };
 
 Header.propTypes = {
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
+  currentUser: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
