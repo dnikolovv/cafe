ab57a6c9cb7da3ce708521cf25ca257446ea0f35
dnikolovv <dnikolovv@hotmail.com>
2019-05-17 14:27:59 +0300

    You can now run the api inside docker.

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker.compose.integration.yml up --exit-code-from cafe.tests
+docker-compose -f docker.compose.integration.yml up --exit-code-from cafe.tests --build
--- server/tests/Cafe.Tests/run-tests-with-coverage-report.sh
+++ server/tests/Cafe.Tests/run-tests-with-coverage-report.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet test Cafe.Tests.csproj -p:Exclude=[xunit*]* -p:CollectCoverage=true -p:CoverletOutputFormat=opencover --logger:"console;verbosity=normal"
+dotnet test Cafe.Tests.csproj -p:Exclude="[xunit*]*%2c[Cafe.Persistance.Migrations]" -p:CollectCoverage=true -p:CoverletOutputFormat=opencover --logger:"console;verbosity=normal"
