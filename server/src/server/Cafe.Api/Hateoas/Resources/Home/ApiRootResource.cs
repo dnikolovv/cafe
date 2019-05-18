039e7d6c4af39e68b5629c421c49319f0743f431
dnikolovv <dnikolovv@hotmail.com>
2019-05-18 11:13:41 +0300

    Slight ApiRootResponse modification.

--- server/src/server/Cafe.Api/Hateoas/Resources/Home/ApiRootResource.cs
+++ server/src/server/Cafe.Api/Hateoas/Resources/Home/ApiRootResource.cs
@@ -1,7 +1,10 @@
-﻿namespace Cafe.Api.Hateoas.Resources.Home
+﻿using Newtonsoft.Json;
+
+namespace Cafe.Api.Hateoas.Resources.Home
 {
     public class ApiRootResource : Resource
     {
+        [JsonIgnore]
         public bool IsUserLoggedIn { get; set; }
     }
 }
